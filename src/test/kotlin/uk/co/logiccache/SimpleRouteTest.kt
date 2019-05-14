4c2322acfb3363ec2a115d176db26a121c41ef91
Matthew Planchant <matt@logiccache.co.uk>
2019-05-14 13:36:51 +0100

    Use JUnit 5.

--- src/test/kotlin/uk/co/logiccache/SimpleRouteTest.kt
+++ src/test/kotlin/uk/co/logiccache/SimpleRouteTest.kt
@@ -1,26 +1,22 @@
 package uk.co.logiccache
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class SimpleRouteTest {
 
+    @Autowired
     lateinit var client: WebTestClient
-
-    @Before
-    fun init() {
-        this.client = WebTestClient.bindToRouterFunction(SimpleRoute().route()).build()
-    }
 
     @Test
     fun hello_should_be_ok() {
