1fa894269143f3074c917cc776949de4328b0985
Matthew Planchant <matthew.planchant@virginmoney.com>
2020-02-03 16:10:58 +0000

    Replaces deprecated 'fromObject'

--- src/main/kotlin/uk/co/logiccache/Routes.kt
+++ src/main/kotlin/uk/co/logiccache/Routes.kt
@@ -2,7 +2,6 @@ package uk.co.logiccache
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 
@@ -11,7 +10,7 @@ class SimpleRoute {
     @Bean
     fun route() = router {
         GET("/hello") {
-            ServerResponse.ok().body(fromObject("Hello World!"))
+            ServerResponse.ok().bodyValue("Hello World!")
         }
     }
 }
